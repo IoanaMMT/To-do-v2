7d14d886b29b06e77d2f0a0adfa51128a38df364
IoanaMMT <34572741+IoanaMMT@users.noreply.github.com>
2024-02-16 06:23:57 +0000

    added style

--- src/to-do-v2.js
+++ src/to-do-v2.js
@@ -9,8 +9,6 @@ import TextField from '@mui/material/TextField';
 export default function MyList(){
     const[newItem, setNewItem] = useState([]);
     const[completedItem, setCompletedItem] = useState([]);
-    const[completedItem, setCompletedItem] = useState([]);
-
 //   caturing the new item and add it to the list
 
 const addItem = (newItemText) => {
@@ -34,92 +32,60 @@ const handleClick = (index) => {
 
 }
 
-
-
-
-// this is an idea how to filter completed items and add it to a list
-// const checkedItems = newItem.filter(item => {if(item.completed = true) {return item}});
-        
-// taking new items and move them to a new state as completed
-        if(updatedItems[index].completed) {
-            setCompletedItem([...completedItem, updatedItems[index]]);
-
-    // filter out the completed items
-           const filteredItems =  updatedItems.filter(item => !item.completed);
-           setNewItem(filteredItems)
-        }
-
-}
-
-
-
-
     return(
         <>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField id="outlined-basic" 
-                            label="Enter new item" 
-                            variant="outlined"
-                            onKeyDown = {
-                                (e) => { if (e.key === "Enter" && e.target.value.trim() !== "") {
-                                     
-                                        e.preventDefault();
-                                    addItem(e.target.value);
-                                    e.target.value = "";
-                                }}
-                                
-                            } /> 
-                            {/* this is to capture the entered new item */}
-            </Box>
-            <div className="itemLists">
-                <div>
-                    <h2>New Items</h2>
-                    <div className="itemLists">
-                <div className="newItemList">
-                    <div>
-                        <h2>New Items</h2>
-                        <FormGroup>
-                                    {newItem.map((item, index) => (
-                                        <FormControlLabel 
-                                        key = {index}
-                                        control={<Checkbox onClick= {() => handleClick(index)} checked={item.completed}/>} 
-                                        label= {item.text}
-                                       />
-                                       ))}
-                                </FormGroup>
+            <div className="hero">
+            <div className="container">
+                <div className="textField">
+                <h1 className="title">Daily Tasks</h1>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '25ch' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        >
+                        <TextField id="outlined-basic" 
+                                    label="Enter new item" 
+                                    variant="outlined"
+                                    onKeyDown = {
+                                        (e) => { if (e.key === "Enter" && e.target.value.trim() !== "") {
+                                            
+                                                e.preventDefault();
+                                            addItem(e.target.value);
+                                            e.target.value = "";
+                                        }}
+                                        
+                                    } /> 
+                                    {/* this is to capture the entered new item */}
+                    </Box>
                 </div>
-                    <div>
-                        <h2>Completed Items</h2>
-                        <ul>
-                            {completedItem.map((item,index) => (
-                                <li key={index}>{item.text}</li>
-                            ))}
-                        </ul>
-                    </div>
-                <div>
-                    {/* filter new items and if completed, move it to a new list of completed items */}
-                </div>
-            </div>
-                    </div>
-                        <div className="completedItemList">
-                            <h2>Completed Items</h2>
-                            <ul className="ulCompletedItemList">
-                                {completedItem.map((item,index) => (
-                                    <li key={index}>{item.text}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    <div>
+                <div className="itemLists">
+                    <div className="newItemList">
+                            <h2>To Do List</h2>
+                            <FormGroup className="ulNewItemList">
+                                        {newItem.map((item, index) => (
+                                            <FormControlLabel 
+                                            key = {index}
+                                            control={<Checkbox onClick= {() => handleClick(index)} checked={item.completed}/>} 
+                                            label= {item.text}
+                                        />
+                                        ))}
+                                    </FormGroup>
                         {/* filter new items and if completed, move it to a new list of completed items */}
+                </div>
+                    <div className="completedItemList">
+                            <h2 className="doneList">Done</h2>
+                                <ul className="ulCompletedItemList">
+                                    {completedItem.map((item,index) => (
+                                        <li key={index}>{item.text}</li>
+                                    ))}
+                                </ul>
                     </div>
                 </div>
+
+           </div>
             </div>
         </>
     )
